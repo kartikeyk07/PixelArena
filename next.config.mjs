28c694adf133ac025a3740aee0e29428f02faf9e
kartikeyk07 <kongekartikey007@gmail.com>
2026-03-31 11:52:02 +0530

    added1

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-=======
-  /* config options here */
->>>>>>> a25c837b2cda02d39f45ac7de799f8988e900966
-  
 };
 
 export default nextConfig;
